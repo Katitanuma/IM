40418fe9a50fd085d1a6576243073724c94a28c1
Marcio Martinez <marciomartinez500@gmail.com>
2017-11-21 02:22:52 -0600

    FORMULARIO DX DEPARTAMENTO

--- INNOVAMASTER/Formularios/FrmDepartamento.vb
+++ INNOVAMASTER/Formularios/FrmDepartamento.vb
@@ -1,15 +1,16 @@
 ﻿Imports System.ComponentModel
 Imports System.Data.SqlClient
+Imports DevExpress.XtraEditors
+
 Public Class FrmDepartamento
     Dim dt As New DataTable
     Dim conec As New Conexion
     Public var As Integer = 0
     Private Sub FrmDepartamento_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        CboBusqueda.Text = CboBusqueda.Items(0).ToString
         MostrarDepartamento()
         GbDepartamento.Enabled = False
         TxtIdDepartamento.Enabled = False
-        DgvDepartamento.Enabled = True
+        GCPrincipal.Enabled = True
         Call LlenarComboboxPais()
 
         Dim NombreArchivo As String = HTMLHelpClass.GetLocalHelpFileName("InnovaMasterAyuda2017.chm")
@@ -17,11 +18,7 @@ Public Class FrmDepartamento
         HelpProvider1.SetHelpNavigator(Me, HelpNavigator.KeywordIndex)
         HelpProvider1.SetHelpKeyword(Me, "Departamento")
     End Sub
-    Private Sub DgvDepartamento_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgvDepartamento.CellClick
-        TxtIdDepartamento.Text = DgvDepartamento.SelectedCells.Item(0).Value
-        TxtDepartamento.Text = DgvDepartamento.SelectedCells.Item(1).Value
-        CboPais.Text = DgvDepartamento.SelectedCells.Item(2).Value
-    End Sub
+
     Private Sub MostrarDepartamento()
         Try
             Dim datos As New FDepartamento
@@ -29,34 +26,33 @@ Public Class FrmDepartamento
 
             If dt.Rows.Count <> 0 Then
                 LblFilas.Visible = False
-                DgvDepartamento.DataSource = dt
+                GCPrincipal.DataSource = dt
 
-                TxtBusqueda.Enabled = True
             Else
                 LblFilas.Visible = True
-                DgvDepartamento.DataSource = Nothing
+                GCPrincipal.DataSource = Nothing
 
-                TxtBusqueda.Enabled = False
             End If
         Catch ex As Exception
             MsgBox(ex.ToString)
         End Try
     End Sub
     Public Sub LlenarComboboxPais()
-        CboPais.Items.Clear()
+        Dim _DT As New DataTable
+        Dim DA As SqlDataAdapter
 
         Try
             conec.Conectarse()
             Dim cmd As New SqlCommand
-            cmd = New SqlCommand("select Pais from Pais")
-            cmd.CommandType = CommandType.Text
+            cmd = New SqlCommand("MostrarPais")
+            cmd.CommandType = CommandType.StoredProcedure
             cmd.Connection = conec.Con
-            Dim dr As SqlDataReader
-            dr = cmd.ExecuteReader
 
-            While dr.Read
-                CboPais.Items.Add(dr(0))
-            End While
+            If cmd.ExecuteNonQuery Then
+                DA = New SqlDataAdapter(cmd)
+                DA.Fill(_DT)
+                CboPais.Properties.DataSource = _DT
+            End If
 
         Catch ex As Exception
             MsgBox(ex.ToString)
@@ -65,37 +61,21 @@ Public Class FrmDepartamento
         End Try
 
     End Sub
-    Private Sub txtBusqueda_TextChanged(sender As Object, e As EventArgs) Handles TxtBusqueda.TextChanged
-        Dim ds As New DataSet
-        Dim dv As New DataView
 
-        ds.Tables.Add(dt.Copy)
-        dv = New DataView(ds.Tables(0))
-
-        dv.RowFilter = CboBusqueda.Text & " like '" & TxtBusqueda.Text & "%'"
-
-        If dv.Count <> 0 Then
-            LblFilas.Visible = False
-            DgvDepartamento.DataSource = dv
-        Else
-            LblFilas.Visible = True
-            DgvDepartamento.DataSource = Nothing
-        End If
-    End Sub
 
 
 
 
     Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles BtnNuevo.Click
         GbDepartamento.Enabled = True
-        TxtIdDepartamento.Clear()
-        TxtDepartamento.Clear()
-        CboPais.Text = Nothing
+        TxtIdDepartamento.EditValue = Nothing
+        TxtDepartamento.EditValue = Nothing
+        CboPais.EditValue = Nothing
         BtnNuevo.Visible = False
         BtnEditar.Visible = False
         BtnInsertar.Visible = True
         BtnCancelar.Visible = True
-        DgvDepartamento.Enabled = False
+        GCPrincipal.Enabled = False
     End Sub
 
     Private Sub BtnEditar_Click(sender As Object, e As EventArgs) Handles BtnEditar.Click
@@ -108,20 +88,21 @@ Public Class FrmDepartamento
             BtnCancelar.Visible = True
             BtnInsertar.Visible = False
             TxtIdDepartamento.Visible = True
-            DgvDepartamento.Enabled = False
+            GCPrincipal.Enabled = False
 
 
         Else
-            MessageBox.Show("Seleccione el Departamento a Actualizar", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            XtraMessageBox.Show("Seleccione el Departamento a Actualizar", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
         End If
     End Sub
 
     Private Sub BtnActualizar_Click(sender As Object, e As EventArgs) Handles BtnActualizar.Click
+
         If TxtDepartamento.Text = Nothing Then
-            MessageBox.Show("Ingrese el Nombre del Departamento", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            XtraMessageBox.Show("Ingrese el Nombre del Departamento", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             TxtDepartamento.Focus()
-        ElseIf CboPais.SelectedItem = Nothing Then
-            MessageBox.Show("Seleccione el País", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        ElseIf CboPais.EditValue = Nothing Then
+            XtraMessageBox.Show("Seleccione el País", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
 
         Else
             Try
@@ -131,31 +112,25 @@ Public Class FrmDepartamento
 
                 datos.gDepartamento = TxtDepartamento.Text
                 datos.gIdDepartamento = CInt(TxtIdDepartamento.Text)
-                Dim cmd As SqlCommand
-                Dim dr As SqlDataReader
-                cmd = New SqlCommand(" Select IdPais from Pais where Pais='" & CboPais.Text & "'")
-                cmd.CommandType = CommandType.Text
-                cmd.Connection = conec.Con
-                dr = cmd.ExecuteReader
 
-                If dr.Read Then
-                    datos.gIdPais = dr(0)
-                End If
+
+                datos.gIdPais = CInt(CboPais.EditValue)
+
 
 
                 If funcion.Actualizar(datos) Then
-                    MessageBox.Show("Departamento Modificado con Exito", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    XtraMessageBox.Show("Departamento Modificado con Exito", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
                     GbDepartamento.Enabled = False
-                    TxtIdDepartamento.Clear()
-                    TxtDepartamento.Clear()
-                    CboPais.Text = Nothing
+                    TxtIdDepartamento.EditValue = Nothing
+                    TxtDepartamento.EditValue = Nothing
+                    CboPais.EditValue = Nothing
                     BtnNuevo.Visible = True
                     BtnEditar.Visible = True
                     BtnInsertar.Visible = False
                     BtnCancelar.Visible = False
                     BtnActualizar.Visible = False
-                    DgvDepartamento.Enabled = True
+                    GCPrincipal.Enabled = True
 
                     MostrarDepartamento()
 
@@ -170,29 +145,29 @@ Public Class FrmDepartamento
     End Sub
 
     Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
-        Dim r As DialogResult = MessageBox.Show("¿Desea Cancelar el Proceso?", "INNOVAMASTER ", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        Dim r As DialogResult = XtraMessageBox.Show("¿Desea Cancelar el Proceso?", "INNOVAMASTER ", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
         If r = DialogResult.Yes Then
             GbDepartamento.Enabled = False
-            TxtIdDepartamento.Clear()
-            TxtDepartamento.Clear()
-            CboPais.Text = Nothing
+            TxtIdDepartamento.EditValue = Nothing
+            TxtDepartamento.EditValue = Nothing
+            CboPais.EditValue = Nothing
             BtnNuevo.Visible = True
             BtnEditar.Visible = True
             BtnInsertar.Visible = False
             BtnCancelar.Visible = False
             BtnActualizar.Visible = False
-            DgvDepartamento.Enabled = True
+            GCPrincipal.Enabled = True
 
         End If
     End Sub
 
     Private Sub Btninsertar_Click(sender As Object, e As EventArgs) Handles BtnInsertar.Click
         If TxtDepartamento.Text = Nothing Then
-            MessageBox.Show("Ingrese el Nombre del Departamento", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            XtraMessageBox.Show("Ingrese el Nombre del Departamento", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             TxtDepartamento.Focus()
-        ElseIf CboPais.SelectedItem = Nothing Then
-            MessageBox.Show("Seleccione el País", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        ElseIf CboPais.EditValue = Nothing Then
+            XtraMessageBox.Show("Seleccione el País", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
 
         Else
             Try
@@ -201,32 +176,21 @@ Public Class FrmDepartamento
                 Dim funcion As New FDepartamento
 
                 datos.gDepartamento = TxtDepartamento.Text
-                Dim cmd As SqlCommand
-                Dim dr As SqlDataReader
-                cmd = New SqlCommand(" Select IdPais from Pais where Pais='" & CboPais.Text & "'")
-                cmd.CommandType = CommandType.Text
-                cmd.Connection = conec.Con
-                dr = cmd.ExecuteReader
-
-                If dr.Read Then
-                    datos.gIdPais = dr(0)
-                End If
-
-
+                datos.gIdPais = CInt(CboPais.EditValue)
 
                 If funcion.Insertar(datos) Then
-                    MessageBox.Show("Departamento Insertado con Exito", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    XtraMessageBox.Show("Departamento Insertado con Exito", "INNOVAMASTER", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
                     GbDepartamento.Enabled = False
-                    TxtIdDepartamento.Clear()
-                    TxtDepartamento.Clear()
-                    CboPais.Text = Nothing
+                    TxtIdDepartamento.EditValue = Nothing
+                    TxtDepartamento.EditValue = Nothing
+                    CboPais.EditValue = Nothing
                     BtnNuevo.Visible = True
                     BtnEditar.Visible = True
                     BtnInsertar.Visible = False
                     BtnCancelar.Visible = False
                     BtnActualizar.Visible = False
-                    DgvDepartamento.Enabled = True
+                    GCPrincipal.Enabled = True
 
                     MostrarDepartamento()
 
@@ -254,12 +218,26 @@ Public Class FrmDepartamento
         End With
     End Sub
 
-
-    Private Sub DgvDepartamento_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgvDepartamento.CellDoubleClick
+    Private Sub GCPrincipal_DoubleClick(sender As Object, e As EventArgs) Handles GCPrincipal.DoubleClick
         If var = 1 Then
             FrmMunicipio.LlenarComboboxDepartamento()
-            FrmMunicipio.CboDepartamento.Text = DgvDepartamento.CurrentRow.Cells(1).Value.ToString
+            FrmMunicipio.CboDepartamento.Text = DgvDepartamento.GetRowCellValue(DgvDepartamento.FocusedRowHandle, ColDepartamento)
             Me.Close()
         End If
+    End Sub
+
+    Private Sub GCPrincipal_Click(sender As Object, e As EventArgs) Handles GCPrincipal.Click
+        Dim index As Integer = DgvDepartamento.FocusedRowHandle
+        TxtIdDepartamento.EditValue = DgvDepartamento.GetRowCellValue(index, ColIdDepartamento)
+        TxtDepartamento.EditValue = DgvDepartamento.GetRowCellValue(index, ColDepartamento)
+        CboPais.Text = DgvDepartamento.GetRowCellValue(index, ColPais)
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
+        GCPrincipal.ShowPrintPreview()
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        Exportar_a_Excel(GCPrincipal, Me.Text)
     End Sub
 End Class
