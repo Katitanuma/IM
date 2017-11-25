e8fa9af753faa49aa8a8624d7de5a0bc2c13757e
Marcio Martinez <marciomartinez500@gmail.com>
2017-11-24 23:08:10 -0600

    Export to PDF

--- INNOVAMASTER/ModuloBloques.vb
+++ INNOVAMASTER/ModuloBloques.vb
@@ -22,4 +22,27 @@
             MessageBox.Show(Ex.Message)
         End Try
     End Sub
+
+    Public Sub Exportar_a_PDF(ByRef Grilla As DevExpress.XtraGrid.GridControl, ByVal Nombre As String)
+        Dim Archivo As New SaveFileDialog
+
+        Try
+            Archivo.Filter = "pdf|*.pdf"
+            Archivo.FileName = Nombre
+            Archivo.ShowDialog()
+            If Archivo.FileName.Length > 0 Then
+
+                Grilla.ExportToPdf(Archivo.FileName)
+
+                ' Lanzar el archivo
+                Process.Start(Archivo.FileName)
+
+            End If
+
+        Catch Ex As Exception
+            Dim _st As New System.Diagnostics.StackTrace()
+            Dim _miFuncion As String = _st.GetFrame(0).GetMethod().Name
+            Mi.ErrorMensaje(Ex.Message, _miFuncion, Me.Name, True)
+        End Try
+    End Sub
 End Module
