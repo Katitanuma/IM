f28710f4de68972d64e735ba36e81c2f13ee66be
Marcio Martinez <marciomartinez500@gmail.com>
2018-02-22 22:49:09 -0600

    No se

--- INNOVAMASTER/Modulos/Modulo1.vb
+++ INNOVAMASTER/Modulos/Modulo1.vb
@@ -1,4 +1,6 @@
-﻿Module Modulo1
+﻿Imports DevExpress.XtraReports.UI
+
+Module Modulo1
     Public Function SHA1(ByVal strToHash As String) As String
         Dim sha1Obj As New Security.Cryptography.SHA1CryptoServiceProvider
         Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
@@ -13,4 +15,15 @@
 
         Return strResult
     End Function
+    Public Sub MostrarReporte(ByVal Titulo As String, ByVal Reporte As XtraReport, ByVal FormMDI As Form, Optional ByVal OcultarMenu As Boolean = False)
+        Dim _printPreview As New FrmModeloReporte
+        With _printPreview
+            .HideToolBars = OcultarMenu
+            .Text = Titulo
+            .MdiParent = FormMDI
+            .Report = Reporte
+            .Show()
+            .Focus()
+        End With
+    End Sub
 End Module
